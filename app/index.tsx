852d219927b0858c2735cc9333486083b175686f
Josh Wilkerson <jgwilkerson@gmail.com>
2025-08-11 23:07:25 -0400

    demo: lesson 1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { styles } from "@/constants/styles"
+import { Text, View } from "react-native"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>👋 Hello.</Text>
     </View>
-  );
+  )
 }
