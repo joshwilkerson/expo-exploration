8780d8741b1e7f52e9a9be63cc2e43eb3bf061fa
Josh Wilkerson <jgwilkerson@gmail.com>
2025-08-12 15:50:02 -0400

    demo: lesson 8

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,10 +7,11 @@ import { HeroImage } from "@/components/hero_image"
 import { IconButton } from "@/components/icon_button"
 import { useTheme } from "@/constants/hooks"
 import { createStyles } from "@/constants/styles"
+import domtoimage from "dom-to-image"
 import * as ImagePicker from "expo-image-picker"
 import * as MediaLibrary from "expo-media-library"
 import { useRef, useState } from "react"
-import { ImageSourcePropType, View } from "react-native"
+import { ImageSourcePropType, Platform, View } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { captureRef } from "react-native-view-shot"
 
@@ -55,18 +56,35 @@ export default function Index() {
   }
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      })
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        })
 
-      await MediaLibrary.saveToLibraryAsync(localUri)
-      if (localUri) {
-        alert("Saved!")
+        await MediaLibrary.saveToLibraryAsync(localUri)
+        if (localUri) {
+          alert("Saved!")
+        }
+      } catch (e) {
+        console.log(e)
       }
-    } catch (e) {
-      console.log(e)
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        })
+
+        let link = document.createElement("a")
+        link.download = "sticker-smash.jpeg"
+        link.href = dataUrl
+        link.click()
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 
